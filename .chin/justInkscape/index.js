cb79cd1b17ea82b0532b5a1178a031d7356502a7
kthjm <toxictoxer@gmail.com>
2017-11-13 17:40:12 +0900

    Modify .chin/justInkscape

--- .chin/justInkscape/index.js
+++ .chin/justInkscape/index.js
@@ -1,44 +1,9 @@
 import Inkscape from "inkscape"
+import envmap from "./envmap.js"
 
-const { ext, config } = envMap.get(process.env.CHIN_ENV)
+const { ext, config } = envmap.get(process.env.CHIN_ENV)
 
 export default (opts) => {
   opts.ext = ext
   return pipe => pipe(new Inkscape(config))
 }
-
-const envMap = new Map([
-  ["white", {
-    ext: ".png",
-    config: [
-      '--export-png',
-      '--export-area-page',
-      '--export-background=#ffffff',
-      '--export-width=1024'
-    ]
-  }],
-  ["trans", {
-    ext: ".png",
-    config: [
-      '--export-png',
-      '--export-area-page',
-      '--export-width=1024'
-    ]
-  }],
-  ["pdf", {
-    ext: ".pdf",
-    config: [
-      '--export-pdf',
-      `--export-area-page`,
-      '--export-width=1024'
-    ]
-  }],
-  ["print", {
-    ext: ".pdf",
-    config: [
-      '--export-pdf',
-      `--export-area-drawing`,
-      '--export-width=1024'
-    ]
-  }]
-])
